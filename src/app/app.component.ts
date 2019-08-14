4b96d43b84b58c6e4e4fcdac30f1f3b8a7d704ae
Partical777 <z25993021@gmail.com>
2019-08-14 20:30:42 +0800

    Change Cat or Dog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   localjson = {
-    "categoryDog" : "狗狗",
-    "categoryCat" : "貓貓"
+    "Dog" : {
+      "category" : "狗狗",
+      "nickname" : "https://www.careline.com.tw/CareLinePet/Pet/assets/images/dog-nickname.gif",
+      "gender" : "https://www.careline.com.tw/CareLinePet/Pet/assets/images/dog-gender.gif",
+      "species" : "https://www.careline.com.tw/CareLinePet/Pet/assets/images/dog-species.gif",
+    },
+    "Cat" : {
+      "category" : "貓貓",
+      "nickname" : "https://www.careline.com.tw/CareLinePet/Pet/assets/images/cat-nickname.gif",
+      "gender" : "https://www.careline.com.tw/CareLinePet/Pet/assets/images/cat-gender.gif",
+      "species" : "https://www.careline.com.tw/CareLinePet/Pet/assets/images/cat-species.gif",
+    }
   }
 
-  categoryChoose = this.localjson.categoryDog;
+  categoryChoose = this.localjson.Dog;
+
+  changeCategory(choose){
+    if(choose == "cat"){
+      this.categoryChoose = this.localjson.Cat;
+    }else if(choose == "dog"){
+      this.categoryChoose = this.localjson.Dog;
+    }
+    console.log(choose);
+  }
 }
