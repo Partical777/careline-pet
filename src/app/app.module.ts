6e78fff8a48f7579447d78b591629d5dd9be50cc
Partical777 <z25993021@gmail.com>
2019-08-15 22:32:33 +0800

    datacheck

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { PetComponent } from './components/pet/pet.component';
 import { MemberComponent } from './components/member/member.component';
+import { DatacheckComponent } from './components/datacheck/datacheck.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, RouterModule.forRoot([
     { path: "", component: PetComponent},
-    { path: "member", component: MemberComponent}
+    { path: "member", component: MemberComponent},
+    { path: "datacheck", component: DatacheckComponent}
   ]) ],
-  declarations: [ AppComponent, PetComponent, MemberComponent ],
+  declarations: [ AppComponent, PetComponent, MemberComponent, DatacheckComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- src/app/components/datacheck/datacheck.component.ts
+++ src/app/components/datacheck/datacheck.component.ts
@@ -1,16 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'member',
-  templateUrl: 'member.component.html',
-  styleUrls: [ 'member.component.css' ]
+  selector: 'datacheck',
+  templateUrl: 'datacheck.component.html',
+  styleUrls: [ 'datacheck.component.css' ]
 })
 
-export class MemberComponent  {
+export class DatacheckComponent  {
   categoryChooseForm = localStorage.getItem("categoryChooseForm");
   nicknameForm = localStorage.getItem("nicknameForm");
   genderChooseForm = localStorage.getItem("genderChooseForm");
   specieChooseForm = localStorage.getItem("specieChooseForm");
   programChoose = localStorage.getItem("programChoose");
-  programMoney = this.programChoose == "安心方案" ? "NT$2,312" : "NT$3,893";
 }
