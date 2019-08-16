b5f15ac28e5efcd8c8a5c3b9a0d442aeb54fbcc1
Partical777 <z25993021@gmail.com>
2019-08-16 14:44:28 +0800

    savetostorage

--- src/app/components/member/member.component.ts
+++ src/app/components/member/member.component.ts
@@ -17,7 +17,9 @@ export class MemberComponent  {
   PetBirthMonth = "";//Added
   PetBirthDay = "";//Added
   PetWeight = "";   //Added
-  PetSeries = "";
+  StartYear = "";//Added
+  StartMonth = "";//Added
+  StartDay = "";//Added
 
 
   LastName = "";//Added
@@ -28,7 +30,10 @@ export class MemberComponent  {
   BirthDay = "";//Added
   Email = "examle@example.com";//Added
   Phone = "";//Added
-  Address = "";
+  Address1 = "";//Added
+  Address2 = "";//Added
+  Address3 = "";//Added
+  Address4 = "";//Added
 
   getYear(event){
     this.BirthYear = event.target.value;
@@ -52,5 +57,36 @@ export class MemberComponent  {
   
   getPetDay(event){
     this.PetBirthDay = event.target.value;
+  }
+
+  getStartYear(event){
+    this.StartYear = event.target.value;
+  }
+
+  getStartMonth(event){
+    this.StartMonth = event.target.value;
+  }
+  
+  getStartDay(event){
+    this.StartDay = event.target.value;
+  }
+
+
+
+
+  SaveToStroage(){
+    localStorage.setItem("Name", this.LastName + this.FirstName);
+    localStorage.setItem("TaiwanID", this.TaiwanID);
+    localStorage.setItem("Birth", this.BirthYear + "/" + this.BirthMonth + "/" + this.BirthDay);
+    localStorage.setItem("Email", this.Email);
+    localStorage.setItem("Phone", this.Phone);
+    localStorage.setItem("Name", this.LastName + this.FirstName);
+    localStorage.setItem("Address", this.Address1 + ", " + this.Address2 + ", " + this.Address3 + ", " + this.Address4);
+
+    localStorage.setItem("PetBirth", this.PetBirthYear + "/" + this.PetBirthMonth + "/" + this.PetBirthDay);
+    localStorage.setItem("PetWeight", this.PetWeight);
+    localStorage.setItem("StartYear", this.StartYear);
+    localStorage.setItem("StartMonth", this.StartMonth);
+    localStorage.setItem("StartDay", this.StartDay);
   }
 }
