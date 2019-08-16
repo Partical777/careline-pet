20cbe2128551b69c40c4824ca320d82396e693a1
Partical777 <z25993021@gmail.com>
2019-08-16 19:15:36 +0800

    Stop here

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
@@ -9,7 +9,7 @@ import { MemberComponent } from './components/member/member.component';
 import { DatacheckComponent } from './components/datacheck/datacheck.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, RouterModule.forRoot([
+  imports:      [ BrowserModule, FormsModule, ReactiveFormsModule, RouterModule.forRoot([
     { path: "", component: PetComponent},
     { path: "member", component: MemberComponent},
     { path: "datacheck", component: DatacheckComponent}
